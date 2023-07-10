14d1df3284911f81795422ac71d786b51d36a359
Lava <63625087+cosmixcom@users.noreply.github.com>
2023-07-10 13:32:43 -0700

    Add files via upload

---
+++ static/index.js
@@ -0,0 +1,55 @@
+const form = document.querySelector('form');
+const input = document.querySelector('input');
+
+form.addEventListener('submit', async event => {
+    event.preventDefault();
+    window.navigator.serviceWorker.register('./sw.js', {
+        scope: __uv$config.prefix
+    }).then(() => {
+        let url = input.value.trim();
+        if (!isUrl(url)) url = 'https://www.google.com/search?q=' + url;
+        else if (!(url.startsWith('https://') || url.startsWith('http://'))) url = 'http://' + url;
+
+
+        window.location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
+        document.addEventListener('keydown', function(event) {
+  if (event.altKey) {
+    window.location.href = 'https://classroom.google.com';
+  }
+});
+    });
+});
+
+function isUrl(val = ''){
+    if (/^http(s?):\/\//.test(val) || val.includes('.') && val.substr(0, 1) !== ' ') return true;
+    return false;
+};
+
+const logoP = [
+  "cifrr",
+  "Cifrr",
+  "cIfrr",
+  "ciFrr",
+  "cifRr",
+  "cifrR",
+  "CIfrr",
+  "cIFrr",
+  "ciFRr",
+  "cifRR",
+  "CIFrr",
+  "cIFRr",
+  "ciFRR",
+  "CIFRr",
+  "cIFRR",
+  "CIFRR"
+];
+
+const logo = document.getElementsByClassName("logo");
+
+function changeLogo() {
+  const random = Math.floor(Math.random() * logoP.length);
+  logo[0].innerHTML = logoP[random];
+  document.title = logoP[random];
+}
+
+setInterval(changeLogo, 100);
